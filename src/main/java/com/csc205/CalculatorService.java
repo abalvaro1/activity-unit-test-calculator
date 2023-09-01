29774ad15c4202da9bc72ba9fbaa6276d6b1e2d9
abalvaro1 <ame2155006@maricopa.edu>
2023-08-31 21:32:43 -0700

    Initial commit

---
+++ src/main/java/com/csc205/CalculatorService.java
@@ -0,0 +1,39 @@
+package com.csc205;
+
+public class CalculatorService {
+
+    private calculator calculator;
+
+    public CalculatorService(calculator calculator) {
+
+        this.calculator = calculator;
+    }
+
+    public int execute(String operation, int a, int b) {
+
+        switch(operation) {
+            case "add":
+                return calculator.add(a, b);
+
+            case "subtract":
+                return calculator.subtract(a, b);
+
+            case "multiply":
+                return calculator.multiply(a, b);
+
+            case "divide":
+                return calculator.divide(a , b);
+
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    public int execute(String operation, int a, int...b) {
+
+        if("add".equals(operation)) {
+            return calculator.add(a, b);
+        }
+        throw new IllegalArgumentException();
+    }
+}
